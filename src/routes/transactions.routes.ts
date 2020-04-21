00e8acc94207eb7423304ca624a7c5e11c2962ee
Beatriz Felix <azz.felix7@gmail.com>
2020-04-21 13:54:23 -0300

    feat(backend): list transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,8 +1,23 @@
 import { Router } from 'express';
 
+import { getCustomRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionsRouter = Router();
+
+transactionsRouter.get('/', async (req, res) => {
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const balance = await transactionsRepository.getBalance();
+  const transactions = await transactionsRepository.find({
+    select: ['id', 'title', 'value', 'type', 'category'],
+    relations: ['category'],
+  });
+
+  return res.json({ transactions, balance });
+});
 
 transactionsRouter.post('/', async (req, res) => {
   const { title, type, value, category } = req.body;
